783953b71ccedb6ff8d70fd16b2332db37b444c9
Rplay <americarplay@gmail.com>
2024-06-29 16:34:50 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -304,7 +304,7 @@ async def start(client:Client, message):
                 file_caption=file.caption
             )
             btn = [[
-                InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream#{file.file_id}')
+                InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream')
             ]]
             toDel = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -341,7 +341,7 @@ async def start(client:Client, message):
         file_caption=files.caption
     )
     btn = [[
-        InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream#{file_id}')
+        InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream')
     ]]
     toDel=await client.send_cached_media(
         chat_id=message.from_user.id,
