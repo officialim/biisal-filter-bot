0f65f9290b19de407e29f27e109ccc52f4cbccb4
officialim <141108672+officialim@users.noreply.github.com>
2024-07-02 00:37:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -114,7 +114,7 @@ async def group_search(client, message):
             return               
         else:
             try: 
-		dc = await message.reply_text(f"{message.text}🎯sᴇᴀʀᴄʜɪɴɢ...") 
+		dc = await message.reply(f"<b><i>⚠️ `{message.text}` searching...</i></b>") 
                 await asyncio.sleep(5)
                 await dc.delete()
                 await auto_filter(client, message)
